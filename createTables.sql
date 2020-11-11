91b49fee2e39e806d540e6d3036f46e147ba8995
Apro123 <akapoor3@ucmerced.edu>
2020-11-11 14:34:53 -0800

    added some columns and modified the entire fake data to match vision

--- createTables.sql
+++ createTables.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS Users;
+DELETE FROM sqlite_sequence WHERE name='Users';
 CREATE TABLE IF NOT EXISTS Users (
     u_id INTEGER PRIMARY KEY AUTOINCREMENT,
     u_firstname VARCHAR(255) NULL,
     u_lastname VARCHAR(255) NULL,
     u_username VARCHAR(255) NULL,
     u_pictureURL VARCHAR(255) NULL,
-    u_email VARCHAR(255) NULL UNIQUE,
+    u_schoolEmail VARCHAR(255) NOT NULL,
     u_schoolID VARCHAR(255) NULL,
     u_password VARCHAR(255) NULL,
     u_genderTitle VARCHAR(31) NULL,
@@ -19,32 +20,36 @@ CREATE TABLE IF NOT EXISTS Users (
 );
 
 DROP TABLE IF EXISTS Programs;
+DELETE FROM sqlite_sequence WHERE name='Programs';
 CREATE TABLE IF NOT EXISTS Programs (
     pr_id INTEGER PRIMARY KEY AUTOINCREMENT,
     pr_name VARCHAR(255) NOT NULL,
     pr_shortName VARCHAR(255) NULL,
-    pr_description VARCHAR(255) NULL,
+    pr_description VARCHAR(1023) NULL,
     pr_logoLocation VARCHAR(255) NULL,
     pr_physicalLocation VARCHAR(255) NULL,
     pr_addedByAdminUserID INT NULL REFERENCES Users(u_id),
     pr_eventID INT NULL REFERENCES Events(e_id),
-    pr_programManagerUserID INT NULL REFERENCES Users(u_id)
+    pr_programManagerUserID INT NULL REFERENCES Users(u_id),
+    pr_tags VARCHAR(255) NULL
 );
 
 DROP TABLE IF EXISTS Events;
+DELETE FROM sqlite_sequence WHERE name='Events';
 CREATE TABLE IF NOT EXISTS Events (
     e_id INTEGER PRIMARY KEY AUTOINCREMENT,
     e_name VARCHAR(255) NOT NULL,
     e_description VARCHAR(255) NULL,
     e_pictureLocation VARCHAR(255) NULL,
     e_programID INT NOT NULL REFERENCES Programs(u_id),
-    e_repeated INT NOT NULL,
+    e_repeated VARCHAR(255) NULL,
     e_date DATE NOT NULL,
     e_tags VARCHAR(255) NULL,
     e_addedBy INT NOT NULL REFERENCES Users(u_id)
 );
 
 DROP TABLE IF EXISTS Posts;
+DELETE FROM sqlite_sequence WHERE name='Posts';
 CREATE TABLE IF NOT EXISTS Posts (
     ps_id INTEGER PRIMARY KEY AUTOINCREMENT,
     ps_addedByUserID INT NOT NULL REFERENCES Users(u_id),
@@ -58,6 +63,7 @@ CREATE TABLE IF NOT EXISTS Posts (
 );
 
 DROP TABLE IF EXISTS Subscriptions;
+DELETE FROM sqlite_sequence WHERE name='Subscriptions';
 CREATE TABLE IF NOT EXISTS Subscriptions (
     s_id INTEGER PRIMARY KEY AUTOINCREMENT,
     s_dateAdded DATE NOT NULL,
@@ -68,6 +74,7 @@ CREATE TABLE IF NOT EXISTS Subscriptions (
 );
 
 DROP TABLE IF EXISTS Items;
+DELETE FROM sqlite_sequence WHERE name='Items';
 CREATE TABLE IF NOT EXISTS Items (
     i_id INTEGER PRIMARY KEY AUTOINCREMENT,
     i_addedBy INT NOT NULL REFERENCES Users(u_id),
@@ -80,24 +87,27 @@ CREATE TABLE IF NOT EXISTS Items (
 );
 
 DROP TABLE IF EXISTS PointOpp;
+DELETE FROM sqlite_sequence WHERE name='PointOpp';
 CREATE TABLE IF NOT EXISTS PointOpp (
     po_id INTEGER PRIMARY KEY AUTOINCREMENT,
     po_name VARCHAR(255) NOT NULL,
     po_description VARCHAR(255) NULL,
     po_type VARCHAR(255) NULL,
     po_externalLink VARCHAR(255) NULL,
-    po_postUniqueID INT NOT NULL REFERENCES Posts(ps_id)
+    po_pointsPossible INT NULL
 );
 
 DROP TABLE IF EXISTS PointHistory;
+DELETE FROM sqlite_sequence WHERE name='PointHistory';
 CREATE TABLE IF NOT EXISTS PointHistory (
     ph_id INTEGER PRIMARY KEY AUTOINCREMENT,
     ph_userID INT NOT NULL REFERENCES Users(u_id),
     ph_pointOppID INT NOT NULL REFERENCES PointOpp(po_id),
-    ph_actualPointsEarned INT NOT NULL
+    ph_actualPointsEarned INT NULL
 );
 
 DROP TABLE IF EXISTS ItemsBought;
+DELETE FROM sqlite_sequence WHERE name='ItemsBought';
 CREATE TABLE IF NOT EXISTS ItemsBought (
     ib_id INTEGER PRIMARY KEY AUTOINCREMENT,
     ib_userID INT NOT NULL REFERENCES Users(u_id),
@@ -107,6 +117,7 @@ CREATE TABLE IF NOT EXISTS ItemsBought (
 );
 
 DROP TABLE IF EXISTS Points;
+DELETE FROM sqlite_sequence WHERE name='Points';
 CREATE TABLE IF NOT EXISTS Points (
     p_id INTEGER PRIMARY KEY AUTOINCREMENT,
     p_userID INT NOT NULL REFERENCES Users(u_id),
--- fillTables.sql
+++ fillTables.sql
@@ -1,106 +1,116 @@
 -- Inserting data into Users
 INSERT INTO Users (u_firstname, u_lastname, u_username, u_pictureURL, u_schoolEmail, u_schoolID, u_password, u_genderTitle, u_professionalTitle, u_affiliatedProgramID, u_adminPriv, u_programPriv, u_lastLoggedIn, u_currentlyLoggedIPAddress, u_addedBy)
 VALUES
-    ('Victoria', 'Preece', 'user #1', NULL, 'vPreece@ucmerced.edu', '802789240', 'password1', NULL, NULL, NULL, 1, 1, NULL, NULL, NULL),
-    ('Payton', 'Greaves', 'user #2', NULL, 'pGreaves@ucmerced.edu', '378502872', 'password2', NULL, NULL, NULL, 1, 1, NULL, NULL, NULL),
-    ('Roisin', 'Tierney', 'user #3', NULL, 'rTierney@ucmerced.edu', '024567491', 'password3', NULL, NULL, NULL, 1, 1, NULL, NULL, NULL),
-    ('Keiren', 'Robin', 'user #4', NULL, 'kRobin@ucmerced.edu', '456872035', 'password4', NULL, NULL, NULL, 0, 1, NULL, NULL, 1),
-    ('Rose', 'Handley', 'user #5', NULL, 'rHandley@ucmerced.edu', '867542098', 'password5', NULL, NULL, NULL, 0, 1, NULL, NULL, 2),
-    ('Phoebe', 'Legge', 'user #6', NULL, 'pLegge@ucmerced.edu', '324587601', 'password6', NULL, NULL, NULL, 0, 1, NULL, NULL, 3),
-    ('Asha', 'Bolton', 'user #7', NULL, 'aBolton@ucmerced.edu', '457822146', 'password7', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL),
-    ('Abubakr', 'Buxton', 'user #8', NULL, 'aBuxton@ucmerced.edu', '145687923', 'password8', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL),
-    ('William', 'Hayes', 'user #9', NULL, 'wHayes@ucmerced.edu', '612358497', 'password9', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL),
-    ('Bjorn', 'Villarreal', 'user #10', NULL, 'bVillarreal@ucmerced.edu', '542031782', 'password10', NULL, NULL, NULL, 0, 0, NULL, NULL, NULL);
+    ('Victoria', 'Preece', 'admin user #1', "https://loremflickr.com/640/360", 'vPreece@ucmerced.edu', '802789240', 'password1', "She/Her", "IT Administrator", NULL, 0, 0, NULL, NULL, NULL),
+    ('Payton', 'Greaves', 'user #2', "https://loremflickr.com/640/360", 'pGreaves@ucmerced.edu', '378502872', 'password2', "She/Her", "Student", NULL, 0, 0, NULL, NULL, NULL),
+    ('Roisin', 'Tierney', 'user #3', "https://loremflickr.com/640/360", 'rTierney@ucmerced.edu', '024567491', 'password3', "He/His/Him", "Student", NULL, 0, 0, NULL, NULL, NULL),
+    ('Keiren', 'Robin', 'user #4', "https://loremflickr.com/640/360", 'kRobin@ucmerced.edu', '456872035', 'password4', "He/His/Him", "Student", NULL, 0, 0, NULL, NULL, ),
+    ('Rose', 'Handley', 'program user #5', "https://loremflickr.com/640/360", 'rHandley@ucmerced.edu', '867542098', 'password5', "She/Her", "Program Intern", NULL, 0, 0, NULL, NULL, 1),
+    (NULL, NULL, NULL, NULL, 'rHandley@ucmerced.edu', NULL, NULL, NULL, NULL, 1, 0, 1, NULL, NULL, 1),
+    (NULL, NULL, NULL, NULL, 'rHandley@ucmerced.edu', NULL, NULL, NULL, NULL, 2, 0, 1, NULL, NULL, 1),
+    (NULL, NULL, NULL, NULL, 'rHandley@ucmerced.edu', NULL, NULL, NULL, NULL, 3, 0, 1, NULL, NULL, 1),
+    ('Phoebe', 'Legge', 'user #6', "https://loremflickr.com/640/360", 'pLegge@ucmerced.edu', '324587601', 'password6', "She/Her", "Student", NULL, 0, 0, NULL, NULL, ),
+    ('Asha', 'Bolton', 'program user #7', "https://loremflickr.com/640/360", 'aBolton@ucmerced.edu', '457822146', 'password7', "She/Her", "Program Intern", NULL, 0, 0, NULL, NULL, 1),
+    (NULL, NULL, NULL, NULL, 'aBolton@ucmerced.edu', NULL, NULL, NULL, NULL, 3, 0, 1, NULL, NULL, 1),
+    (NULL, NULL, NULL, NULL, 'aBolton@ucmerced.edu', NULL, NULL, NULL, NULL, 4, 0, 1, NULL, NULL, 1),
+    ('Abubakr', 'Buxton', 'user #8', "https://loremflickr.com/640/360", 'aBuxton@ucmerced.edu', '145687923', 'password8', "She/Her", "Student", NULL, 0, 0, NULL, NULL, NULL),
+    ('William', 'Hayes', 'program user #9', "https://loremflickr.com/640/360", 'wHayes@ucmerced.edu', '612358497', 'password9', "He/His/Him", "Program Intern", NULL, 0, 0, NULL, NULL, 1),
+    (NULL, NULL, NULL, NULL, 'wHayes@ucmerced.edu', NULL, NULL, NULL, NULL, 5, 0, 1, NULL, NULL, 1),
+    ('Bjorn', 'Villarreal', 'user #10', "https://loremflickr.com/640/360", 'bVillarreal@ucmerced.edu', '542031782', 'password10', "He/His/Him", "Student", NULL, 0, 0, NULL, NULL, 1);
 
 -- Inserting data into Programs
-INSERT INTO Programs (pr_name, pr_shortName, pr_description, pr_logoLocation, pr_physicalLocation, pr_addedByAdminUserID, pr_eventID, pr_programManagerUserID)
+INSERT INTO Programs (pr_name, pr_shortName, pr_description, pr_logoLocation, pr_physicalLocation, pr_addedByAdminUserID, pr_eventID, pr_programManagerUserID, pr_tags)
 VALUES
-    ('program #1', NULL, NULL, NULL, NULL, 1, 2, 1),
-    ('program #2', NULL, NULL, NULL, NULL, 2, 1, 5),
-    ('program #3', NULL, NULL, NULL, NULL, 3, 3, 4),
-    ('program #4', NULL, NULL, NULL, NULL, 1, 6, 4),
-    ('program #5', NULL, NULL, NULL, NULL, 2, 4, 1),
-    ('program #6', NULL, NULL, NULL, NULL, 3, 5, 2);
+    ('program #1', "pr1", "des1", "https://loremflickr.com/640/360", "COB 1 RM 251", 1, NULL, 5, "IT, entertainment"),
+    ('program #2', "pr2", "des2", "https://loremflickr.com/640/360", "COB 1 RM 252", 1, NULL, 5, "food, fun"),
+    ('program #3', "pr3", "des3", "https://loremflickr.com/640/360", "COB 1 RM 253", 1, NULL, 5, "thrilling, exciting"),
+    ('program #3', NULL, NULL, NULL, NULL, NULL, NULL, 10, NULL),
+    ('program #4', "pr4", "des4", "https://loremflickr.com/640/360", "COB 1 RM 254", 1, NULL, 10, "food, entertainment"),
+    ('program #4', NULL, NULL, NULL, NULL, NULL, 5, NULL, NULL),
+    ('program #5', "pr5", "des5", "https://loremflickr.com/640/360", "COB 1 RM 255", 1, NULL, 14, "fun, exciting"),
+    ('program #5', NULL, NULL, NULL, NULL, NULL, 5, NULL, NULL);
 
 -- Inserting data into Events
 INSERT INTO Events (e_name, e_description, e_pictureLocation, e_programID, e_repeated, e_date, e_tags, e_addedBy)
 VALUES
-    ('event #1', NULL, NULL, 2, 0, '2020-11-18', NULL, 2),
-    ('event #2', NULL, NULL, 1, 0, '2020-12-05', NULL, 1),
-    ('event #3', NULL, NULL, 3, 0, '2020-11-21', NULL, 3),
-    ('event #4', NULL, NULL, 5, 0, '2020-11-21', NULL, 2),
-    ('event #5', NULL, NULL, 6, 0, '2020-11-21', NULL, 3),
-    ('event #6', NULL, NULL, 4, 0, '2020-11-21', NULL, 1);
+    ('event #1', "des1", "https://loremflickr.com/640/360", 1, "Weekly", '2020-11-18', NULL, 1),
+    ('event #2', "des2", "https://loremflickr.com/640/360", 2, NULL, '2020-12-05', NULL, 1),
+    ('event #3', "des3", "https://loremflickr.com/640/360", 3, NULL, '2020-11-21', NULL, 5),
+    ('event #4', "des4", "https://loremflickr.com/640/360", 3, "Monthly", '2020-8-21', NULL, 10),
+    ('event #5', "des5", "https://loremflickr.com/640/360", 7, NULL, '2020-10-21', NULL, 10),
+    ('event #6', "des6", "https://loremflickr.com/640/360", 5, "Biweekly", '2020-9-21', NULL, 14);
 
 -- Inserting data into Posts
 INSERT INTO Posts (ps_addedByUserID, ps_name, ps_description, ps_pictureLocation, ps_eventID, ps_pointOpportunity, ps_tags, ps_dateAdded)
 VALUES
-    (1, 'post #1', NULL, NULL, 2, 30, NULL, '2020-11-15'),
-    (2, 'post #2', NULL, NULL, 1, 30, NULL, '2020-11-16'),
-    (3, 'post #3', NULL, NULL, 3, 30, NULL, '2020-11-16'),
-    (2, 'post #4', NULL, NULL, 4, 30, NULL, '2020-11-17'),
-    (3, 'post #5', NULL, NULL, 6, 30, NULL, '2020-11-19'),
-    (1, 'post #6', NULL, NULL, 5, 30, NULL, '2020-11-20');
+    (1, 'post #1', "des1", "https://loremflickr.com/640/360", 1, 1, "student, food", '2020-11-15'),
+    (1, 'post #2', "des2", "https://loremflickr.com/640/360", 5, 2, "faculty, entertainment", '2020-11-16'),
+    (1, 'post #3', "des3", "https://loremflickr.com/640/360", 6, 3, "student, fun", '2020-11-16'),
+    (1, 'post #4', "des4", "https://loremflickr.com/640/360", 3, 4, "student, food", '2020-11-17'),
+    (10, 'post #5', "des5", "https://loremflickr.com/640/360", 3, 5, "student, entertainment", '2020-11-18'),
+    (5, 'post #6', "des6", "https://loremflickr.com/640/360", 1, 6, "student, food", '2020-11-19'),
+    (5, 'post #7', "des7", "https://loremflickr.com/640/360", 2, 1, "faculty, entertainment", '2020-11-20');
+    (10, 'post #8', "des8", "https://loremflickr.com/640/360", 4, 2, "student, fun", '2020-11-22');
+    (14, 'post #9', "des9", "https://loremflickr.com/640/360", 5, 3, "faculty, food", '2020-11-24');
+    (10, 'post #10', "des10", "https://loremflickr.com/640/360", 6, 4, "student, entertainment", '2020-11-25');
+    (5, 'post #11', "des11", "https://loremflickr.com/640/360", 3, 5, "faculty, fun", '2020-11-28');
+    (5, 'post #12', "des12", "https://loremflickr.com/640/360", 4, 6, "student, food", '2020-11-29');
 
 -- Inserting data into Subscriptions
 INSERT INTO Subscriptions (s_dateAdded, s_userID, s_programID, s_eventID, s_sendPushNotification)
 VALUES
-    ('2020-11-15', 3, 2, 1, 0),
-    ('2020-11-16', 2, 1, 2, 0),
-    ('2020-11-17', 2, 3, 3, 0),
-    ('2020-11-19', 1, 5, 4, 0),
-    ('2020-11-20', 4, 6, 5, 0),
-    ('2020-11-20', 1, 4, 6, 0);
+    ('2020-11-15', 2, 1, NULL, 0),
+    ('2020-11-16', 3, 2, NULL, 1),
+    ('2020-11-17', 4, 3, NULL, 1),
+    ('2020-11-19', 5, 5, NULL, 0),
+    ('2020-11-20', 9, 7, NULL, 1),
+    ('2020-11-20', 16, 3, NULL, 0);
+    ('2020-11-20', 16, NULL, 6, 1);
 
 -- Inserting data into Items
 INSERT INTO Items (i_addedBy, i_name, i_description, i_picture, i_pointsWorth, i_quantitiy, i_programID)
 VALUES
-    (1, 'item #1', NULL, NULL, 15, 10, 2),
-    (3, 'item #2', NULL, NULL, 250, 3, 1),
-    (4, 'item #3', NULL, NULL, 100, 5, 4),
-    (2, 'item #4', NULL, NULL, 50, 10, 1),
-    (5, 'item #5', NULL, NULL, 30, 15, 5),
-    (2, 'item #6', NULL, NULL, 75, 8, 3);
+    (1, 'item #1', "des1", "https://loremflickr.com/640/360", 15, 10, 1),
+    (5, 'item #2', "des2", "https://loremflickr.com/640/360", 250, 3, 3),
+    (10, 'item #3', "des3", "https://loremflickr.com/640/360", 100, 5, 5),
+    (14, 'item #4', "des4", "https://loremflickr.com/640/360", 50, 10, 7),
+    (1, 'item #5', "des5", "https://loremflickr.com/640/360", 30, 15, 3),
+    (5, 'item #6', "des6", "https://loremflickr.com/640/360", 75, 8, 2);
 
 -- Inserting data into PointOpp
-INSERT INTO PointOpp (po_name, po_description, po_type, po_externalLink, po_postUniqueID)
+INSERT INTO PointOpp (po_name, po_description, po_type, po_externalLink, po_pointsPossible)
 VALUES
-    ('survey #1', NULL, NULL, NULL, 1),
-    ('survey #2', NULL, NULL, NULL, 2),
-    ('survey #3', NULL, NULL, NULL, 3),
-    ('survey #4', NULL, NULL, NULL, 4),
-    ('survey #5', NULL, NULL, NULL, 5),
-    ('survey #6', NULL, NULL, NULL, 6);
+    ('survey #1', "des1", "survey", "forms.google.com", 30),
+    ('survey #2', "des2", "survey", "forms.google.com", 25),
+    ('survey #3', "des3", "survey", "forms.google.com", 25),
+    ('survey #4', "des4", "survey", "forms.google.com", 50),
+    ('survey #5', "des5", "survey", "forms.google.com", 25),
+    ('survey #6', "des6", "survey", "forms.google.com", 40);
 
 -- Inserting data into PointHistory
 INSERT INTO PointHistory (ph_userID, ph_pointOppID, ph_actualPointsEarned)
 VALUES
-    (4, 1, 30),
-    (5, 2, 20),
-    (6, 3, 30),
-    (7, 4, 18),
-    (8, 5, 25),
-    (9, 6, 30);
+    (2, 1, NULL),
+    (3, 2, 20),
+    (4, 3, NULL),
+    (4, 4, 50),
+    (5, 4, 20),
+    (9, 5, NULL),
+    (16, 6, 35);
 
 -- Inserting data into ItemsBought
 INSERT INTO ItemsBought (ib_userID, ib_itemID, ib_dateOrdered, ib_dateReceived)
 VALUES
-    (7, 5, '2020-12-1', NULL),
-    (6, 5, '2020-12-3', NULL),
-    (5, 1, '2020-12-3', NULL),
-    (8, 1, '2020-12-4', NULL),
-    (9, 1, '2020-12-6', NULL),
-    (4, 5, '2020-12-9', NULL);
+    (4, 4, '2020-12-1', '2020-12-3'),
+    (16, 1, '2020-12-3', '2020-12-4'),
+    (16, 1, '2020-12-5', NULL),
 
 -- Inserting data into Points
 INSERT INTO Points (p_userID, p_currentPoints, p_totalPoints)
 VALUES
-    (1, 0, 0),
     (2, 0, 0),
-    (3, 0, 0),
-    (4, 0, 30),
-    (5, 15, 30),
-    (6, 0, 30),
-    (7, 0, 30),
-    (8, 15, 30),
-    (9, 15, 30);
+    (3, 0, 20),
+    (4, 0, 50),
+    (5, 0, 20),
+    (9, 0, 0),
+    (16, 5, 30),
