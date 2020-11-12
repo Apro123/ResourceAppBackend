780b1fdc1c6310b594e0e53dfa85cac16f89573e
Apro123 <akapoor3@ucmerced.edu>
2020-11-11 16:46:02 -0800

    modified package.json. more setup in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var dt = datetime.create(); //dt.now()
 var cors = require('cors');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
 app.use(cors());
 app.use(express.json());
 
@@ -32,11 +32,46 @@ var db = new sqlite3.Database('rapp.db');
 
 //app.post ....
 
+async function dbq(q) {
+  return await new Promise(function(resolve, reject) {
+    let sql = ``;
+    if(q == 1) {
+      sql = `SELECT ps_id FROM Posts
+      WHERE ps_id IN
+      (
+        SELECT ps_id FROM Posts, Events, Programs, Subscriptions, Users WHERE ps_eventID = e_id AND e_programID = pr_id AND pr_id = s_programID AND s_userID = u_id
+        INTERSECT
+        SELECT ps_id FROM Posts, Events, Subscriptions, Users WHERE ps_eventID = e_id AND s_eventID = e_id AND s_userID = u_id
+      )
+      ORDER BY ps_dateAdded LIMIT 10 OFFSET 0
+        `;
+    }
+    if(q == 2) {
+      sql = ``;
+    }
+    if(q == 3) {
+      sql = ``;
+    }
+    db.all(sql, [], (err,rows) => {
+      console.log(err);
+      resolve(rows);
+    });
+  });
+}
+
+app.get("/test", async function(req, res) {
+  console.log(req.body['query']);
+  var d = await dbq(req.body['query'])
+  res.send({
+    "status": "success",
+    "data": d
+  });
+});
 
 app.get("/", function(req, res) {
   res.send({
     "status": "success",
-    "data": "Hello world!"
+    "data": "Visit /q# for each of the 20 queries to execute"
   });
 });
 
